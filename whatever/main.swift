965dea7564df05afdd343a3e7629a96bbf95124f
cstaker <cstaker@coupeville.k12.wa.us>
2016-01-06 10:32:52 -0800

    Initial Commit

---
+++ whatever/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  whatever
+//
+//  Created by Cody Staker on 1/6/16.
+//  Copyright © 2016 Cody Staker. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
